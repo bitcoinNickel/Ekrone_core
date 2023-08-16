d2941c6342c14d297df159830e8b9b0291a4c391
bitcoinnickel <48751960+bitcoinNickel@users.noreply.github.com>
2023-08-16 23:34:11 +0800

    initial

--- src/config/cryptonote_config.h
+++ src/config/cryptonote_config.h
@@ -71,7 +71,8 @@ namespace cryptonote
         * You should see your premine appear in the previously generated wallet.
 
         */
-        const char GENESIS_COINBASE_TX_HEX[] = "70e8f7fcffffffffffff010000210178c0f9eddbb74e1c372847f00e051e7c1c5e4cd5b776b7bf34c970e32e8e4182";
+        const char GENESIS_COINBASE_TX_HEX[] = "010a01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210142694232c5b04151"
+                                               "d9e4c27d31ec7a68ea568b19488cfcb422659a07a0e44dd5";
         static_assert(sizeof(GENESIS_COINBASE_TX_HEX) / sizeof(*GENESIS_COINBASE_TX_HEX) != 1, "GENESIS_COINBASE_TX_HEX must not be empty.");
 
         /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
