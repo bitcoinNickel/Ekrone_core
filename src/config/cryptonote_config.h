cbede6fbe6aaecec541c4bf2e85af59bd4140d66
bitcoinnickel <48751960+bitcoinNickel@users.noreply.github.com>
2026-07-22 02:54:59 +0200

    Update cryptonote_config.h
    fork height

--- src/config/cryptonote_config.h
+++ src/config/cryptonote_config.h
@@ -159,11 +159,10 @@ namespace cryptonote
         const size_t FUSION_TX_MIN_INPUT_COUNT = 12;
         const size_t FUSION_TX_MIN_IN_OUT_COUNT_RATIO = 4;
 
-        const uint32_t UPGRADE_HEIGHT_V2 = 1;
-        const uint32_t UPGRADE_HEIGHT_V3 = 2;
-        const uint32_t UPGRADE_HEIGHT_V4 = 3; // Upgrade height for CN-Lite Variant 1 switch.
-        const uint32_t UPGRADE_HEIGHT_V5 = 4; // Upgrade height for CN-Turtle Variant 2 switch.
-        const uint32_t UPGRADE_HEIGHT_CURRENT = UPGRADE_HEIGHT_V5;
+        const uint32_t UPGRADE_HEIGHT_V2 = 100000;
+        const uint32_t UPGRADE_HEIGHT_V3 = 200000;
+        const uint32_t UPGRADE_HEIGHT_V4 = 300000;
+        const uint32_t UPGRADE_HEIGHT_V5 = 400000;
 
         const unsigned UPGRADE_VOTING_THRESHOLD = 90;                             // percent
         const uint32_t UPGRADE_VOTING_WINDOW = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY; // blocks
